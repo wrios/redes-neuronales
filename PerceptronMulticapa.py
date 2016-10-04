eba78f1c4dc1a17dc5fe8aa369a0314a752c96e0
Agustín Gallardo <agallardo@agallardo-desktop.Home>
2016-10-04 17:20:01 -0300

    corregido codigo. Falta corregir tamaños de matriz.

--- PerceptronMulticapa.py
+++ PerceptronMulticapa.py
@@ -6,8 +6,30 @@ import numpy as np
 #variables con el tamanio de las capas intermedias.
 m = 11
 l = 5
+lr = 0.15
 
-def activation(W, X, Y):
+#Matrices con los pesos desde una capa a la siguiente. (los pesos que inciden en una neurona J desde una neurona I).
+#generados en matrices con numeros aleatorios entre 0 y 1.
+W = [np.random.rand(11, m+1), np.random.rand(m+1, l+1), np.random.rand(l+1, 1)]
+#deltaW = [np.random.rand(11, m+1), np.random.rand(m+1, l+1), np.random.rand(l+1, 1)]
+
+#Agrego +1 en todas las matrices que quedan del lado derecho de las multiplicaciones para representar los umbrales
+#de cada neurona (cada neurona es representada por una columna)
+
+#Vector de entrada (1x10) que contiene los datos de una instancia
+#A leer de archivo CSV
+X = np.zeros(11)
+
+deltaW = [np.zeros((11, m+1)), np.zeros((m+1, l+1)), np.zeros((l+1, 1))]
+
+Y = [np.zeros(m+1), np.zeros(l+1), np.zeros(1)]
+Yprima = [np.zeros(m), np.zeros(l), np.zeros(1)]
+
+
+def activation():
+	global W
+	global X
+	global Y
 #Le aplico la funcion de activacion a todas las neuronas paso a paso haciendo F(resultado de multiplicacion matricial)
 
 #Agrego -1 en todos los vectores que esten del lado izquierdo de una multiplicacion
@@ -18,18 +40,34 @@ def activation(W, X, Y):
 #Calculos de multiplicacion de matrices para obtener resultado
 #AGREGAR -1 en el final del vector para que haga la cuenta con el umbral
 	Y[0] = nonlin(np.dot(X, W[0]))
-	Y[0][m] = -1
+	b = np.full((Y[0][0].size,1), -1)
+	np.concatenate((Y[0], b), axis=0)
+	print Y[0]
+
+	Yprima[0] = np.delete(Y[0], m, 0)
+	print Yprima[0]
 
 #AGREGAR -1 en el final del vector para que haga la cuenta con el umbral
 	Y[1] = nonlin(np.dot(Y[0], W[1]))
-	Y[1][l] = -1
+	b = np.full((Y[1][0].size,1), -1)
+	np.concatenate((Y[1], b), axis=0)
+	
+	Yprima[1] = np.delete(Y[1], l, 0)
 
+#Resultado final
 	Y[2] = nonlin(np.dot(Y[1], W[2]))
+
+	#Yprima[2] = np.delete(Y[2], 1, 0)
 
 	return Y[2]
 
 
-def batch(W, deltaW, X, Y):
+def batch():
+	global W
+	global deltaW
+	global X
+	global Y
+	
 	e = 0
 	with open(sys.argv[1], 'rb') as csvfile:
 		dataset = csv.reader(csvfile, delimiter=',', quotechar='\'')
@@ -41,29 +79,33 @@ def batch(W, deltaW, X, Y):
 			else:
 				esperado = 0
 			X = [float(row[1]), float(row[2]), float(row[3]), float(row[4]), float(row[5]), float(row[6]), float(row[7]), float(row[8]), float(row[9]), float(row[10]), -1]
-			X = normalizar(X)
-			activation(W, X, Y)
+			X = normalizar()
+			activation()
 
-			e = e + correction(Y, deltaW, esperado)
+			e = e + correction(esperado)
 
-	adaptation(W, deltaW)
+	adaptation()
 	return e
 
-def correction(Y, deltaW, esperado):
+def correction(esperado):
+	global Y
+	global deltaW
 #Diferencia entre el resultado obtenido y el esperado	
 	E = Y[2] - esperado
 #Se usa el modulo y se lo eleva al cuadrado, porque el error cuadratico medio es buen indicador	
 	e = np.power(np.absolute(E),2)
 
-	for j in range(3, 1):
+	for j in range(1, 3):
 		
-		D = E * nonlin( np.dot(Y[j-1], W[j]), true)
+		D = E * nonlin( np.dot(Yprima[3-j-1], W[3-j]), True)
 
-		deltaW[j] = deltaW[j] + lr * np.dot(Y[j-1].T, D)
+		deltaW[3-j] = deltaW[3-j] + lr * np.dot(Yprima[3-j-1].T, D)
 
 	return e
 
-def adaptation(W, deltaW):
+def adaptation():
+	global W
+	global deltaW
 	for j in range(1, 3):
 		W[j] = W[j] + deltaW[j]
 		deltaW[j] = 0
@@ -76,7 +118,8 @@ def nonlin(x,deriv=False):
     return 1/(1+np.exp(-x))
 
 
-def interpretar(Y):
+def interpretar():
+	global Y
 #interpretacion del resultado en letras
 	if (Y[2] > 0.5):
 		diagnostico = "B"
@@ -85,7 +128,8 @@ def interpretar(Y):
 	
 	return diagnostico
 
-def normalizar(X):
+def normalizar():
+	global X
 	X[0] = (X[0] - 18556.3594594595) / 4445.9064834242
 	X[1] = (X[1] - 21288.2324324324) / 6164.3937442774
 	X[2] = (X[2] - 113925.113513514) / 28603.5114501387
@@ -100,35 +144,25 @@ def normalizar(X):
 
 def main():
 
-	#Matrices con los pesos desde una capa a la siguiente. (los pesos que inciden en una neurona J desde una neurona I).
-	#generados en matrices con numeros aleatorios entre 0 y 1.
-
-	#Agrego +1 en todas las matrices que quedan del lado derecho de las multiplicaciones para representar los umbrales
-	#de cada neurona (cada neurona es representada por una columna)
-
-	#Vector de entrada (1x10) que contiene los datos de una instancia
-	#A leer de archivo CSV
-	X = np.zeros(11)
-
-	W = [np.random.rand(11, m+1), np.random.rand(m+1, l+1), np.random.rand(l+1, 1)]
-	#deltaW = [np.random.rand(11, m+1), np.random.rand(m+1, l+1), np.random.rand(l+1, 1)]
-	deltaW = [np.zeros((11, m+1)), np.zeros((m+1, l+1)), np.zeros((l+1, 1))]
-
-	Y = [np.zeros(m+1), np.zeros(l+1), np.zeros(1)]
 
 
 	#Sin entrenar
 	X = [11779, 29321, 93649, 1300312, 2469, 1071, 4459, 6299, 586, 1506, -1]
-	X = normalizar(X)
-	activation(W, X, Y)
-	print interpretar(Y)
+	normalizar()
+	activation()
+	print interpretar()
+	
 	#Entrenamiento
-	print batch(W, deltaW, X, Y)
-	X = [11779, 29321, 93649, 1300312, 2469, 1071, 4459, 6299, 586, 1506, -1]
-	X = normalizar(X)
+	n = 0
+	error = 1
+	while error >= 0.15 or n <= 1000:
+		error = batch()
+	
 	#Post Entrenamiento
-	activation(W, X, Y)
-	print interpretar(Y)
+	X = [11779, 29321, 93649, 1300312, 2469, 1071, 4459, 6299, 586, 1506, -1]
+	normalizar()
+	activation()
+	print interpretar()
 
 if __name__ == "__main__":
     main()
