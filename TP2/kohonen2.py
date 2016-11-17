b04674f7389713e4d89759502fe7586c04aa535b
wrios@dc.uba.ar <kennedy.wrc@hotmail.com>
2016-11-17 19:10:05 -0300

    sigma y vecindario listo

--- TP2/kohonen2.py
+++ TP2/kohonen2.py
@@ -13,15 +13,16 @@ import numpy as np
 import ast
 import scipy.io
 from numpy import linalg as LA
+import os.path
+import matplotlib.pylab as plt
 
-TotalEpoca = 2000
-m1 = 12
-m2 = 12
+TotalEpoca = 550
+m1 = 8
+m2 = 8
 W = []
 X = np.ones((856,1))
 Y = np.zeros((856,1))
 M = []
-WP = []
 Map = np.zeros((m1, m2))
 dmax = m1 #lo suficientemente grande para poder abarcar todas las neuronas y luego solo quede la ganadora
 dmin = 0.55 # hace que vaya mas lento o rapido el aprendizaje
@@ -32,9 +33,10 @@ in_cargarEntrenamiento = False
 in_entrenamiento = True
 in_guardarEntrenamiento = True
 in_dataset = 'tp2_training_dataset.csv'
+fname = 'kohonen_'+str(dmin)+'_'+str(TotalEpoca)+'_'+str(in_inicio_validacion)+'_'+str(in_fin_validacion)
 
 def learningRate(n):
-	lr = 0.05/np.power(n+16,1.8)
+	lr = 0.05/np.power(n+16,0.95)
 	return lr
 
 
@@ -63,9 +65,14 @@ def armarMapaActivaciones():
 
 def gaussiana(n):
 	r =  (dmax*((dmin/dmax)**(n/TotalEpoca)))
-	if r < 1 and r > 0.3:
+	if r < 1 and r > 0.4:
 		r = 1
 	return r 	
+
+def sigma(n):
+	dr = (m1-1)/(n*m1)
+	sigma = m1/(1+(n*m1*dr))	
+	return sigma 
 
 def armarMatriz():
 	global W
@@ -76,7 +83,7 @@ def armarMatriz():
 			fila.append(np.random.uniform( -0.1, 0.1, (856,1)))
 		W.append(fila)
 
-def activar(X):
+def activar():
 	global Y
 	norm = np.linalg.norm(W[0][0] - X)
 	Y[:] = (W[0][0] - X)
@@ -93,8 +100,8 @@ def activar(X):
 				j = j_t
 	return i, j, Y_t
 
-def rangoVecindad(n, i, j, i_t ,j_t):
-	return np.exp((-((np.power(i-i_t,2)+(np.power(j-j_t,2)))/(2*(np.power(gaussiana(n),2))))))
+def rangoVecindad(n, i, j, i_t ,j_t,sigma):
+	return np.exp((-((np.power(i-i_t,2)+(np.power(j-j_t,2)))/(2*(np.power(sigma,2))))))
 
 def vecindad(n, i, j, Y_t):
 	r = [] 
@@ -104,16 +111,15 @@ def vecindad(n, i, j, Y_t):
 			t1 = i_t
 			t2 = j_t
 			if t1 < 0:
-				t1 += m1
+				t1 += (2*m1)
 			if t1 > m1-1:
 				t1 = t1%(m1-1)	
 			if t2 < 0:
-				t2 += m1
+				t2 += (2*m1)
 			if t2 > m1-1:
 				t2 = t2%(m1-1)	 	
-		#	print rangoVecindad(n, i, j, t1, t2)
 			if not(i == t1 and j == t2):
-				x = [t1, t2, (X - W[t1][t2])]#*rangoVecindad(n, i, j, t1, t2)]
+				x = [t1, t2, (X - W[t1][t2])*rangoVecindad(n, i, j, t1, t2, sigma(n))]
 			else:
 				x = [t1, t2, Y_t] 
 			r.append(x)		
@@ -134,20 +140,16 @@ def epoca(n):
 	global W
 	global Y
 	vecindades = []
-	for vector in WP:
-		i, j, Y_t = activar(vector)
+	reader = csv.reader(open(in_dataset,'rb'))
+	for vector in reader:
+		X[:] = np.asarray(vector[1:]).reshape((856,1))
+		i, j, Y_t = activar()
 		v = vecindad(n, i, j, Y_t)
 		vecindades.append(v)
 	correccion(vecindades, n)
 
 def entrenamiento():
-	global WP
 	n = 1
-	WP = []
-	reader = csv.reader(open(in_dataset,'rb'))
-	for vector in reader:
-		X[:] = np.asarray(vector[1:]).reshape((856,1))
-		WP.append(X)
 	while n < TotalEpoca:
 		progress(n, TotalEpoca)
 		epoca(n)
@@ -163,14 +165,17 @@ def cicloCompleto():
 	for vector in reader:
 		#if not(int(in_inicio_validacion) <= e_actual <= int (in_fin_validacion)):
 		X[:] = np.asarray(vector[1:]).reshape((856,1))
-		i, j, Y_t = activar(X)
+		i, j, Y_t = activar()
 		M[i][j].append(vector[0])
 		progress(l, 900, 'i: '+str(i)+'j: '+str(j)+'vector[0]: '+vector[0])
 		l += 1
 
 def graficar():
+	fig = plt.figure()
+	fig.suptitle('kohonen'+fname)
 	print Map
 	plt.matshow(Map)
+	plt.savefig('training/'+fname+'.png', format = 'png')
 	plt.show()
 
 def most_common(L):
@@ -183,19 +188,36 @@ def most_common(L):
 		return -1
 
 def guardarEntrenamiento():
-	outfile = open('entrenamiento_Kohonen'+str(TotalEpoca)+'_'+str(in_inicio_validacion)+'_'+str(in_fin_validacion), 'w')
-	np.save(outfile, W)
-	outfile.close()
+	if not(checkFile('training/'+fname)):
+		print 'Guardando Entrenamiento'
+		outfile = open('training/'+fname, 'w')
+		np.save(outfile, W)
+		outfile.close()
+		print 'Entrenamiento Guardado'
 
 def cargarEntrenamiento():
 	global W
-	infile = open('entrenamiento_Kohonen'+str(TotalEpoca)+'_'+str(in_inicio_validacion)+'_'+str(in_fin_validacion), 'r')
-	W[:] = np.load(infile)
-	infile.close()
+	if checkFile('training/'+fname):
+		print 'Cargando Entrenamiento'
+		infile = open('training/'+fname, 'r')
+		W[:] = np.load(infile)
+		infile.close()
+		print 'Entrenamiento Cargado'
+		return True
+	return False
+
+def checkFile(fname):
+	if os.path.exists(fname):
+		return os.path.isfile(fname)
+	else:
+		return False
 
 
 def main():
+	global fname
+	global dmin
 	global in_dataset
+	global TotalEpoca
 	global in_cargarEntrenamiento
 	global in_entrenamiento
 	global in_guardarEntrenamiento
@@ -210,11 +232,18 @@ def main():
 			return 0
 		else:
 			in_dataset = sys.argv[1]
-			in_cargarEntrenamiento = sys.argv[2] == '1'
-			in_entrenamiento = sys.argv[3] == '1'
-			in_guardarEntrenamiento = sys.argv[4] == '1'
-			in_inicio_validacion = sys.argv[5]
-			in_fin_validacion = sys.argv[6]
+			#dmin es para la funcion gaussiana, velocidad con la que decrece el vecindario(no explicitamente,
+			# ya que hay otros parametros involucrados )
+			dmin = float(sys.argv[2])
+			#cantidad total de epocas
+			TotalEpoca = int(sys.argv[3])
+			in_cargarEntrenamiento = sys.argv[4] == '1'
+			in_entrenamiento = sys.argv[5] == '1'
+			in_guardarEntrenamiento = sys.argv[6] == '1'
+			in_inicio_validacion = sys.argv[7]
+			in_fin_validacion = sys.argv[8]
+	fname = 'kohonen_'+str(dmin)+'_'+str(TotalEpoca)+'_'+str(in_inicio_validacion)+'_'+str(in_fin_validacion)
+
 	if in_cargarEntrenamiento:
 		print 'Comienza Cargar Entrenamiento'
 		cargarEntrenamiento()
