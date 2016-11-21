23d4c39bbc306dce930544fc3d086b7f5a1654d1
wrios@dc.uba.ar <kennedy.wrc@hotmail.com>
2016-11-21 20:39:09 -0300

    exp.py

--- TP2/exp.py
+++ TP2/exp.py
@@ -2,95 +2,48 @@ import os
 
 
 print 'bloque 0 '
-print 'dmin,0.05'
-os.system("python kohonen.py tp2_training_dataset.csv 0.05 400 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.05 600 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.05 800 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.05 1000 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.05 1200 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.05 1400 0 1 1 0 0")
+print '7x7'
+os.system("python kohonen.py 7 7 400 ")
+os.system("python kohonen.py 7 7 600 ")
+os.system("python kohonen.py 7 7 800 ")
+os.system("python kohonen.py 7 7 1000 ")
+os.system("python kohonen.py 7 7 1200 ")
+os.system("python kohonen.py 7 7 1400 ")
 
 
 print 'bloque 1 '
-print 'dmin,0.15'
-os.system("python kohonen.py tp2_training_dataset.csv 0.15 400 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.15 600 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.15 800 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.15 1000 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.15 1200 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.15 1400 0 1 1 0 0")
+print '9x9'
+os.system("python kohonen.py 9 9 400 ")
+os.system("python kohonen.py 9 9 600 ")
+os.system("python kohonen.py 9 9 800 ")
+os.system("python kohonen.py 9 9 1000 ")
+os.system("python kohonen.py 9 9 1200 ")
+os.system("python kohonen.py 9 9 1400 ")
 
 print 'bloque 2 '
-print 'dmin,0.25'
-os.system("python kohonen.py tp2_training_dataset.csv 0.25 400 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.25 600 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.25 800 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.25 1000 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.25 1200 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.25 1400 0 1 1 0 0")
+print '11x11'
+os.system("python kohonen.py 11 11 400 ")
+os.system("python kohonen.py 11 11 600 ")
+os.system("python kohonen.py 11 11 800 ")
+os.system("python kohonen.py 11 11 1000 ")
+os.system("python kohonen.py 11 11 1200 ")
+os.system("python kohonen.py 11 11 1400 ")
 
 
 print 'bloque 3 '
-print 'dmin,0.35'
-os.system("python kohonen.py tp2_training_dataset.csv 0.35 400 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.35 600 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.35 800 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.35 1000 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.35 1200 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.35 1400 0 1 1 0 0")
+print '13x13'
+os.system("python kohonen.py 13 13 400 ")
+os.system("python kohonen.py 13 13 600 ")
+os.system("python kohonen.py 13 13 800 ")
+os.system("python kohonen.py 13 13 1000 ")
+os.system("python kohonen.py 13 13 1200 ")
+os.system("python kohonen.py 13 13 1400 ")
 
 print 'bloque 4 '
-print 'dmin,0.45'
-os.system("python kohonen.py tp2_training_dataset.csv 0.45 400 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.45 600 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.45 800 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.45 1000 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.45 1200 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.45 1400 0 1 1 0 0")
-
-
-print 'bloque 5 '
-print 'dmin,0.55'
-os.system("python kohonen.py tp2_training_dataset.csv 0.55 400 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.55 600 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.55 800 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.55 1000 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.55 1200 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.55 1400 0 1 1 0 0")
-
-
-print 'bloque 6 '
-print 'dmin,0.65'
-os.system("python kohonen.py tp2_training_dataset.csv 0.65 400 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.65 600 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.65 800 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.65 1000 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.65 1200 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.65 1400 0 1 1 0 0")
-
-print 'bloque 7 '
-print 'dmin,0.75'
-os.system("python kohonen.py tp2_training_dataset.csv 0.75 400 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.75 600 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.75 800 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.75 1000 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.75 1200 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.75 1400 0 1 1 0 0")
-
-print 'bloque 8 '
-print 'dmin,0.85'
-os.system("python kohonen.py tp2_training_dataset.csv 0.85 400 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.85 600 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.85 800 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.85 1000 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.85 1200 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.85 1400 0 1 1 0 0")
-
-print 'bloque 9 '
-print 'dmin,0.95'
-os.system("python kohonen.py tp2_training_dataset.csv 0.95 400 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.95 600 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.95 800 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.95 1000 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.95 1200 0 1 1 0 0")
-os.system("python kohonen.py tp2_training_dataset.csv 0.95 1400 0 1 1 0 0")
+print '15x15'
+os.system("python kohonen.py 15 15 400 ")
+os.system("python kohonen.py 15 15 600 ")
+os.system("python kohonen.py 15 15 800 ")
+os.system("python kohonen.py 15 15 1000 ")
+os.system("python kohonen.py 15 15 1200 ")
+os.system("python kohonen.py 15 15 1400 ")
